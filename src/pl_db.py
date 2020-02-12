79cf0c91e7b1c25b0e7fbf34257392282cf77b6d
ulises-jeremias <ulisescf.24@gmail.com>
2020-02-12 01:36:40 -0300

    Added scripts

---
+++ src/pl_db.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import pandas as pd
+
+db = ""
+df = pd.read_csv("/tf/data/heart.csv")
+
+# change column names
+df.columns = ['age', 'sex', 'chest_pain_type', 'resting_blood_pressure',
+              'cholesterol', 'fasting_blood_sugar', 'rest_ecg',
+              'max_heart_rate_achieved', 'exercise_induced_angina',
+              'st_depression', 'st_slope', 'num_major_vessels',
+              'thalassemia', 'target']
+
+columns_whitelist = ['age', 'chest_pain_type', 'resting_blood_pressure',
+                     'cholesterol', 'exercise_induced_angina', 'st_depression',
+                     'st_slope', 'num_major_vessels', 'thalassemia']
+                    
+for i, patient in df.iterrows():
+    for (k, v) in patient.items():
+        if k in columns_whitelist:
+            db += "symptom(p{}, {}, {})\n".format(i, k, v)
+    db += "\n"
+
+f = open("/tf/results/db.pl", "w")
+f.write(db)
+f.close()
